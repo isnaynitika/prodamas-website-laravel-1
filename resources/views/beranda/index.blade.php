7ff348c4b52312d3f0aedb151937abec5103ce51
ukhara <ukhara@student.ub.ac.id>
2021-12-21 14:39:50 +0700

    upd ber

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -25,29 +25,35 @@
 
 <body>
 
-  <!--Banner-->
-  <div class="banner" style="text-shadow: 8px 8px 10px black;">
-    <video id="background-video" autoplay loop muted poster="kota.png">
-      <source src="img/kedirikota.mp4" type="video/mp4" />
-    </video>
-    <h1>PRODAMAS PLUS</h1>
+  <!-- awal jumbotron -->
+  <div class="container-fluid banner">
+    <div class="container banner-content">
+      <h1>Bidang</h1>
+    </div>
   </div>
-  <!--end Banner-->
+  <!-- akhir jumbotrom -->
 
   <!--Tentang-->
-  <section id="tentang">
+  <section class="tentang">
     <div class="container">
-      <div class="row mt-5">
-        <div class="col text-center">
-          <h2 style="font-family: Inter, sans-serif">Tentang</h2>
+      <div class="row">
+        <div class="col mt-5 mb-5 text-center">
+          <h2>Tentang Prodamas</h2>
+        </div>
+        <div class="col">
         </div>
       </div>
-      <div class="row mt-3" style="font-family: Montserrat, sans-serif">
-        <div class="col text-center">
-          <h4>
-            Program pembangunan dan pemberdayaan masyarakat, terobosan Pemerintah Kota Kediri yang ditujukan untuk mendorong partisipasi
-            dan peran aktif masyarakat dalam pembangunan.
-          </h4>
+      <div class="row">
+        <div class="col">
+          <p style="text-align: justify;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div class="col">
+          <img src="{{url('../img/PRODAMASPLUS.png')}}" width="600px" height="300px ">
         </div>
       </div>
     </div>
@@ -56,10 +62,10 @@
 
   <!--artikel-->
   <div class="container artikel mb-5 mt-5">
-    <h3 class="mb-3 text-center" style="font-family: Inter, sans-serif">Informasi Terupdate</h3>
+    <h3 class="mb-4" style="font-family: Inter, sans-serif">Artikel</h3>
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
-        <div class="card h-100">
+        <div class="card h-100 hover-card">
           <img src="img/slide3.jpeg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Launching Kampung Keren</h5>
@@ -68,7 +74,7 @@
         </div>
       </div>
       <div class="col">
-        <div class="card h-100">
+        <div class="card h-100 hover-card">
           <img src="img/slide4.jpeg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Kampung Keren</h5>
@@ -77,7 +83,7 @@
         </div>
       </div>
       <div class="col">
-        <div class="card h-100">
+        <div class="card h-100 hover-card">
           <img src="img/slide5.jpeg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Prodamas</h5>
@@ -94,142 +100,125 @@
   </div>
   <!--end artikel-->
 
-  <!--carousel-->
-  <div class="container-fluid carousel-contain py-5 mt-5">
-    <div class="container">
-      <div id="carouselExampleCaptions" class="carousel slide col-lg-8 offset-lg-2" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="img/slide1.jpeg" class="d-block w-100" alt="Kampung Keren">
-          </div>
-          <div class="carousel-item">
-            <img src="img/slide2.jpeg" class="d-block w-100" alt="Kampung Keren">
-          </div>
-          <div class="carousel-item">
-            <img src="img/slide3.jpeg" class="d-block w-100" alt="Kampung Keren">
-          </div>
-          <div class="carousel-item">
-            <img src="img/slide4.jpeg" class="d-block w-100" alt="Kampung Keren">
-          </div>
-          <div class="carousel-item">
-            <img src="img/slide5.jpeg" class="d-block w-100" alt="Kampung Keren">
+  <!--video-->
+  <div class="container artikel mb-5 mt-5">
+    <h3 class="mb-4" style="font-family: Inter, sans-serif">Video</h3>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide3.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Launching Kampung Keren</h5>
+            <p class="card-text">Dana untuk pemberdayaan warga di level RT di Kota Kediri melalui program pemberdayaan masyarakat (Prodamas) plus bakal dikucurkan.</p>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+      </div>
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide4.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Kampung Keren</h5>
+            <p class="card-text"> Di mana alokasi anggaran Prodamas Plus mencapai Rp 100 juta per RT per tahun yang akan mencakup enam bidang kegiatan. Pemkot Kediri Kucurkan Rp 100 Juta Per RT.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide5.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Prodamas</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+        </div>
       </div>
     </div>
+    <a href="{{ url('bincangprodamas') }}">
+      <h6 class="text-end mt-3"><u>Info Lainnya <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+          </svg></u></h6>
+    </a>
   </div>
-  <!--end carousel-->
+  <!--end video-->
 
-  <!--persentase-->
-  <section id="persentase">
-    <div class="container mt-5">
-      <div class="row text-center justify-content-center m-5">
-        <h3 class="mb-5">Persentase Pembangunan Prodamas Plus 2021</h3>
-        <div class="col-sm-6 col-md-4 mb-4">
-          <div class="img">
-            <img src="img/1.png" class="rounded-circle" alt="1" width="150" height="150" />
-            <div class="img-body">
-              <h5 class="img-title mt-4">Ekonomi</h5>
-            </div>
+  <!--foto-->
+  <div class="container artikel mb-5 mt-5">
+    <h3 class="mb-4" style="font-family: Inter, sans-serif">Foto</h3>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide3.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Launching Kampung Keren</h5>
+            <p class="card-text">Dana untuk pemberdayaan warga di level RT di Kota Kediri melalui program pemberdayaan masyarakat (Prodamas) plus bakal dikucurkan.</p>
           </div>
         </div>
-        <div class="col-sm-6 col-md-4 mb-4">
-          <div class="img">
-            <img src="img/2.png" class="rounded-circle" alt="2" width="150" height="150" />
-            <div class="img-body">
-              <h5 class="img-title mt-4">Kesehatan</h5>
-            </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide4.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Kampung Keren</h5>
+            <p class="card-text"> Di mana alokasi anggaran Prodamas Plus mencapai Rp 100 juta per RT per tahun yang akan mencakup enam bidang kegiatan. Pemkot Kediri Kucurkan Rp 100 Juta Per RT.</p>
           </div>
         </div>
-        <div class="col-sm-6 col-md-4 mb-4">
-          <div class="img">
-            <img src="img/3.png" class="rounded-circle" alt="3" width="150" height="150" />
-            <div class="img-body">
-              <h5 class="img-title mt-4">Sosial Budaya</h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 mb-4">
-          <div class="img">
-            <img src="img/4.png" class="rounded-circle" alt="4" width="150" height="150" />
-            <div class="img-body">
-              <h5 class="img-title mt-4">Infrastruktur</h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 mb-4">
-          <div class="img">
-            <img src="img/5.png" class="rounded-circle" alt="5" width="150" height="150" />
-            <div class="img-body">
-              <h5 class="img-title mt-4">Kepemudaan</h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 mb-4">
-          <div class="img">
-            <img src="img/6.png" class="rounded-circle" alt="6" width="150" height="150" />
-            <div class="img-body">
-              <h5 class="img-title mt-4">Pendidikan</h5>
-            </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide5.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Prodamas</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
           </div>
         </div>
       </div>
     </div>
-  </section>
-  <!--end Persentase-->
-
-  <!--potensi kecamatan-->
-  <div class="container-fluid">
-    <div class="container mt-5">
-      <div class="row justify-content-center">
-        <div class="col-sm-6 col-md-3 hover-card mb-4">
-          <div class="card">
-            <img src="img/kota.png" class="card-img-top rounded" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Kecamatan Kota</h5>
-              <p class="card-text">Berada di sebelah timur Sungai Brantas Kecamatan Kota bisa dibilang merupakan jantung dari Kota Kediri.</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-3 hover-card mb-4">
-          <div class="card">
-            <img src="img/mojoroto.png" class="card-img-top rounded" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Kecamatan Mojoroto</h5>
-              <p class="card-text">Terletak di sebelah barat aliran Sungai Brantas dan di lereng Gunung Klotok dan Maskumambang.</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-3 hover-card mb-4">
-          <div class="card">
-            <img src="img/pesantren.png" class="card-img-top rounded" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Kecamatan Pesantren</h5>
-              <p class="card-text">Pesantren adalah sebuah kecamatan di Kota Kediri, Provinsi Jawa Timur, Indonesia.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <a href="{{ url('bincangprodamas') }}">
+      <h6 class="text-end mt-3"><u>Info Lainnya <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+          </svg></u></h6>
+    </a>
   </div>
-  <!--end potensi kecamatan-->
+  <!--end foto-->
+
+  <!--audio-->
+  <div class="container artikel mb-5 mt-5">
+    <h3 class="mb-4" style="font-family: Inter, sans-serif">Audio</h3>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide3.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Launching Kampung Keren</h5>
+            <p class="card-text">Dana untuk pemberdayaan warga di level RT di Kota Kediri melalui program pemberdayaan masyarakat (Prodamas) plus bakal dikucurkan.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide4.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Kampung Keren</h5>
+            <p class="card-text"> Di mana alokasi anggaran Prodamas Plus mencapai Rp 100 juta per RT per tahun yang akan mencakup enam bidang kegiatan. Pemkot Kediri Kucurkan Rp 100 Juta Per RT.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 hover-card">
+          <img src="img/slide5.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Prodamas</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a href="{{ url('bincangprodamas') }}">
+      <h6 class="text-end mt-3"><u>Info Lainnya <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+          </svg></u></h6>
+    </a>
+  </div>
+  <!--end audio-->
 
   <!--berlangganan-->
   <div class="container py-5 mb-5">
