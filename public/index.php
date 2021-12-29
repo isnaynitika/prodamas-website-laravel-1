db219c274fa987daf1d8ba6c417c037a5ab49db0
Diyah Anggraini <anggrainidiyah25@gmail.com>
2021-12-29 23:57:46 +0700

    Update index.php
    coba benerin cpanel in index.php DIR

--- public/index.php
+++ public/index.php
@@ -17,7 +17,8 @@ define('LARAVEL_START', microtime(true));
 */
 
 if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+//     require __DIR__.'/../storage/framework/maintenance.php';
+     require __DIR__.'/../project/vendor/autoload.php';
 }
 
 /*
@@ -44,7 +45,8 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// $app = require_once __DIR__.'/../bootstrap/app.php';
+ $app = require_once __DIR__.'/../project/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
