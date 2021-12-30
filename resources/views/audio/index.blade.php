cb991100100b019b6b561fb03470daa9ce36249d
samuelchrist12 <christiawansamuel@gmail.com>
2021-12-30 14:14:22 +0700

    audio

--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -68,14 +68,20 @@
                     </div>
                     <div class="modal-body">
                         <div class="row row-cols-1 row-cols-md-3 mb-2 g-4 audioPlayerBox centerItms">
-                            <div class="card audioIsi">
+                            <div class="card" style="
+                                width: 35rem;
+                                border: none;
+                                ">
                                 <img src="{{ asset('/audioProd/thumb/'.$audio->gambar_sampul) }}" class="d-flex justify-content"  style="width: 100%;" alt="...">
                                 <audio controls style="width: 100%;">
                                     <source src="{{ asset('/audioProd/fileaudio/'.$audio->slug) }}" type="audio/mpeg">
                                     Your browser does not support the audio element.
                                 </audio>
                             </div>
-                            <div class="card audioIsi">
+                            <div class="card" style="
+                            width: 40rem;
+                            border: none;
+                            ">>
                                 <h1> {{$audio->judul}}</h1>
                                 <p>{{$audio->konten}}</p>
                             </div>
