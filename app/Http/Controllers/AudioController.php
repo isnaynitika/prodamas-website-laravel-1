b310015b70ce57f915ece82543ee8bbc38765084
samuelchrist12 <christiawansamuel@gmail.com>
2021-12-30 12:11:14 +0700

    audio

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AudioController extends Controller
 {
     public function index(){
-        // $audios = Audio::all();
-        return view('audio.index');
+        $audios = Audio::all();
+        return view('audio.index',$audios->audios);
     }
 }
