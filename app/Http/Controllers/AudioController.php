2038fdde1d67d2c99427a51a6a2203503a2c3281
samuelchrist12 <christiawansamuel@gmail.com>
2021-12-30 13:05:23 +0700

    audio

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Audio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class AudioController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         // $audios = Audio::all();
         $audios = DB::table('audios')->get();
-        return view('audio.index',['audios'=>$audios]);
+        return view('video.index',['audios'=>$audios]);
     }
 }
