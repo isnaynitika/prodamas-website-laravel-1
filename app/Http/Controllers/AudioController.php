d088cc7a38ce61c2a1aa52aac32bdccd79e18461
samuelchrist12 <christiawansamuel@gmail.com>
2021-12-30 13:03:19 +0700

    audio

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -9,6 +9,6 @@ class AudioController extends Controller
     public function index(){
         // $audios = Audio::all();
         $audios = DB::table('audios')->get();
-        return view('audio.index');
+        return view('audio.index',['audios'=>$audios]);
     }
 }
