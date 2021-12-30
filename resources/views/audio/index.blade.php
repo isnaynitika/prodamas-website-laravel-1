4b61626f5df1caa4ef6563143af5859d53947431
samuelchrist12 <christiawansamuel@gmail.com>
2021-12-30 14:08:24 +0700

    audio

--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -68,7 +68,7 @@
                     </div>
                     <div class="modal-body">
                         <div class="row row-cols-1 row-cols-md-3 mb-2 g-4 audioPlayerBox centerItms">
-                            <div class="card audioThumbModal"style="width: 30rem;">
+                            <div class="card audioThumbModal">
                                 <img src="{{ asset('/audioProd/thumb/'.$audio->gambar_sampul) }}" class="d-flex justify-content"  style="width: 100%;" alt="...">
                                 <audio controls style="width: 100%;">
                                     <source src="{{ asset('/audioProd/fileaudio/'.$audio->slug) }}" type="audio/mpeg">
