459c7ba78d1c84c9f0845eade1871e4915bcef0b
Diyah Anggraini <anggrainidiyah25@gmail.com>
2022-01-06 06:41:19 +0700

    benerin css

--- resources/views/tentang/pokmas.blade.php
+++ resources/views/tentang/pokmas.blade.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 
     <!--CSS-->
-    <link rel="stylesheet" href="{{asset('css/pokmas.css')}}">
+    <link rel="stylesheet" href={{ HTML::style('css/pokmas.css') }}>
 </head>
 
 <body>
