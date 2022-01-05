030cd0807d99f36d441617ea18772ad46cca1fdd
samuelchrist12 <christiawansamuel@gmail.com>
2022-01-05 22:29:53 +0700

    artikel

--- routes/web.php
+++ routes/web.php
@@ -86,11 +86,6 @@ Route::post('beranda.index', [NewsletterController::class, 'store']);
 
 //artikel
 Route::resource('artikel', artikelController::class);
-
-Route::get('/layoutartikel/', function () {
-    return view('artikel.layout');
-});
-
 //Route::post('add_prosess', 'artikelController@add_process');
 Route::get('/artikel', [artikelController::class,'index']);
 
