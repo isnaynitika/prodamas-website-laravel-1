0e46b65cfdc83142e0028227f94230dd0246d533
samuelchrist12 <christiawansamuel@gmail.com>
2022-01-05 23:01:15 +0700

    artikel

--- resources/views/artikel/layout.blade.php
+++ resources/views/artikel/layout.blade.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-{{-- <head>
+<head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -21,7 +21,7 @@
   <!--CSS-->
   <link rel="stylesheet" href="css/artikel.css">
 
-</head> --}}
+</head>
 
 <body>
 <!-- Content -->
