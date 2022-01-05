50fa3045fe1e3dde181c73fccfcf74b1f2a69c40
samuelchrist12 <christiawansamuel@gmail.com>
2022-01-05 23:07:03 +0700

    artikel

--- resources/views/artikel/artikelLay.blade.php
+++ resources/views/artikel/artikelLay.blade.php
@@ -19,7 +19,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 
   <!--CSS-->
-  <link rel="stylesheet" href="css/artikel.css">
+  <link rel="stylesheet" href="{{ asset('/css/artikel.css') }}">
 
 </head>
 
