1ee989d9764dab54779a893f9ae30c7a5b2d6bdc
samuelchrist12 <christiawansamuel@gmail.com>
2022-01-05 23:08:46 +0700

    artikel

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 
     <!--CSS-->
-    <link rel="stylesheet" href="css/desain.css">
+    <link rel="stylesheet" href="{{ asset('css/desain.css') }}">
 
 </head>
 
