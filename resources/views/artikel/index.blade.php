e342cdc0fa145925fa73de69e19d8c8f60cae342
Diyah Anggraini <anggrainidiyah25@gmail.com>
2022-01-11 11:59:17 +0700

    artikel css

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -28,7 +28,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 
     <!--CSS-->
-    <link rel="stylesheet" href="css/kampung.css">
+    <link rel="stylesheet" href="css/artikel.css">
 </head>
 
 <body>
