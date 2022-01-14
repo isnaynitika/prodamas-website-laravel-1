e889e9617d76b38a437ad88f5902d0374fa39df6
Diyah Anggraini <anggrainidiyah25@gmail.com>
2022-01-14 11:23:29 +0700

    search hilang

--- resources/views/tentang/pokmas.blade.php
+++ resources/views/tentang/pokmas.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.desain')
 @section('main-tentang','active')
 @section('pokmas','active')
-@section('search')
+
 <form class="d-flex">
     <input class="form-control me-2" type="search" style="font-family: 'Font Awesome 5 Free'; font-weight: 600; border-radius: 30px; height: 30px" placeholder=" &#xf002;" aria-label="Search" />
 </form>
-@endsection
+
 @section('content')
 <!DOCTYPE html>
 <html lang="en">
